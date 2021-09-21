2a198474bd2f5a89d85ed3b9069a395a69e7b9cf
hoang <giahoangth@gmail.com>
2021-09-21 09:01:44 +0700

    chore: log behavior

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
 import pytest
+import logging
 from faker import Faker
 
+logger = logging.getLogger()
 
-@pytest.fixture(scope="module",autouse=True)
+
+@pytest.fixture(scope="function",autouse=True)
 def dumb_fixture(faker: Faker):    
+    logger.critical(faker)
+    pass
+
+
+@pytest.fixture(scope="session",autouse=True)
+def dumb_fixture1(faker: Faker):    
+    logger.critical(faker)
     pass
 
 
 def test_something(faker):    
-    print(faker)
+    logger.critical(faker)
+    # expect The faker fixture will return the session-scoped instance    
+    pass
+
+
+def test_somethingelse(faker):    
+    logger.info(faker)
     # expect The faker fixture will return the session-scoped instance    
     pass
